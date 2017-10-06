63b8178fef342955e4fe1567486e993bcf7b75d6
Felipe Barboza <fbs@poli.ufrj.br>
2017-10-06 00:44:15 -0300

    Script to plot the BER results has been added

--- equalizer/berScripts/plotResults.m
+++ equalizer/berScripts/plotResults.m
@@ -9,73 +9,40 @@ addpath(['.' filesep 'results']);
 
 
 
-load testSM_PAPA_LinEqBer.mat;
-figure
-for i = 1:size(ber,2)
-    semilogy(SNR,ber(:,i))
-    hold on
+fileVector = {'resultsBER1','resultsBER2'};
+
+for k = 1:length(fileVector)
+
+    load([fileVector{k} '.mat']);
+    figure;
+
+    h = plot(randn(10,1));
+    availableMarkers = set( h, 'Marker' );
+
+    for j = 1:size(ber,1)
+
+
+        for i = 1:size(ber,2)
+            h = semilogy(SNR,squeeze(ber(j,5,:)));
+            h.Marker = availableMarkers{i};
+            hold on
+
+        end
+
+    end
+
+    xlabel('SNR [dB]','interpreter','latex');
+    ylabel('BER','interpreter','latex');
+
+    H = legend('$\bar{\gamma}_{\mathrm{NL}} = \bar{\gamma}_{\mathrm{L}}$','$\bar{\gamma}_{\mathrm{NL}} = 1.5\bar{\gamma}_{\mathrm{L}}$','$\bar{\gamma}_{\mathrm{NL}} = 2\bar{\gamma}_{\mathrm{L}}$',...
+        '$\bar{\gamma}_{\mathrm{NL}} = 2.5\bar{\gamma}_{\mathrm{L}}$','$\bar{\gamma}_{\mathrm{NL}} = 3\bar{\gamma}_{\mathrm{L}}$','$\bar{\gamma}_{\mathrm{NL}} = 3.5\bar{\gamma}_{\mathrm{L}}$',...
+        '$\bar{\gamma}_{\mathrm{NL}} = 4\bar{\gamma}_{\mathrm{L}}$');
+
+    set(H,'interpreter','latex','location','SouthWest')
+    xlim([min(SNR) max(SNR)]);
+    ylim([1e-8 1]);
+    grid on
 end
-xlabel('SNR [dB]','interpreter','latex');
-ylabel('BER','interpreter','latex');
-
-H = legend('MI = 0.05','MI = 0.075','MI = 0.1');
-
-set(H,'interpreter','latex','location','SouthWest')
-xlim([min(SNR) max(SNR)]);
-ylim([1e-8 1]);
-
-
-
-
-load testSM_PAPA_VolterraEqBer.mat;
-figure
-for i = 1:size(ber,2)
-    semilogy(SNR,ber(:,i))
-    hold on
-end
-xlabel('SNR [dB]','interpreter','latex');
-ylabel('BER','interpreter','latex');
-
-H = legend('MI = 0.05','MI = 0.075','MI = 0.1');
-
-set(H,'interpreter','latex','location','SouthWest')
-xlim([min(SNR) max(SNR)]);
-ylim([1e-8 1]);
-
-
-
-
-load testSM_PAPA_DFELinBer.mat;
-figure
-for i = 1:size(ber,2)
-    semilogy(SNR,ber(:,i))
-    hold on
-end
-xlabel('SNR [dB]','interpreter','latex');
-ylabel('BER','interpreter','latex');
-
-H = legend('MI = 0.05','MI = 0.075','MI = 0.1');
-
-set(H,'interpreter','latex','location','SouthWest')
-xlim([min(SNR) max(SNR)]);
-ylim([1e-8 1]);
-
-
-
-load testSM_PAPA_DFEVolterraBer.mat;
-figure
-for i = 1:size(ber,2)
-    semilogy(SNR,ber(:,i))
-    hold on
-end
-xlabel('SNR [dB]','interpreter','latex');
-ylabel('BER','interpreter','latex');
-
-H = legend('MI = 0.05','MI = 0.075','MI = 0.1');
-
-set(H,'interpreter','latex','location','SouthWest')
-xlim([min(SNR) max(SNR)]);
-ylim([1e-8 1]);
 
 
 
